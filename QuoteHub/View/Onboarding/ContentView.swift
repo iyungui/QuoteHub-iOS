f4e6e6718c6c1a94c252098bc3eaa96466cfe15e
iyungui <rsdbddml@gmail.com>
2024-10-06 00:14:16 +0900

    Add Assets

--- QuoteHub/View/Onboarding/ContentView.swift
+++ QuoteHub/View/Onboarding/ContentView.swift
@@ -26,22 +26,67 @@ import GoogleSignInSwift
 //}
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
         VStack {
-            GoogleSignInButton(action: handleSignInButton)
+            HStack(spacing: 20) {
+                Button(action: {
+                    handleSignInWithApple()
+                }) {
+                    Image("appleIcon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                }
+                .buttonStyle(PlainButtonStyle())
+
+                Button(action: {
+                    handleSignInWithGoogle()
+                }) {
+                    Image("googleIcon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                }
+                .buttonStyle(PlainButtonStyle())
+
+                Button(action: {
+                    handleSignInWithKakao()
+                }) {
+                    Image("kakaoIcon")
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
+                        .frame(width: 50, height: 50)
+                        .background(Color(hex: "ffe812"))
+                        .clipShape(.rect(cornerRadius: 5))
+                }
+            }
         }
+        .padding()
     }
     
-    private func handleSignInButton() {
-        guard let rootViewController = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController else {return}
+    private func handleSignInWithApple() {
+        print("APLLE LOGIN")
+    }
+    
+    private func handleSignInWithGoogle() {
+        print("GOOGLE LOGIN")
         
-        GIDSignIn.sharedInstance.signIn(
-            withPresenting: rootViewController) { signInResult, error in
-                guard let result = signInResult else {
-                    // Inspect error
-                    return
-                }
-                // If sign in succeeded, display the app's main content View.
-            }
+//        guard let rootViewController = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController else {return}
+//        
+//        GIDSignIn.sharedInstance.signIn(
+//            withPresenting: rootViewController) { signInResult, error in
+//                guard let result = signInResult else {
+//                    // Inspect error
+//                    return
+//                }
+//                // If sign in succeeded, display the app's main content View.
+//            }
+    }
+    
+    private func handleSignInWithKakao() {
+        print("KAKAO LOGIN")
     }
 }
