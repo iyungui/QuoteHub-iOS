3c27b12c98ae415323c4afcccd915d13f131beec
iyungui <rsdbddml@gmail.com>
2025-05-25 21:27:13 +0900

    [fix] google sign in 기존 코드 제거 - 원래 코드로 복구

--- QuoteHub/QuoteHubApp.swift
+++ QuoteHub/QuoteHubApp.swift
@@ -6,25 +6,15 @@
 //
 
 import SwiftUI
-import GoogleSignIn
 
 @main
 struct QuoteHubApp: App {
-//    @StateObject var userAuthManager = UserAuthenticationManager()
+    @StateObject var userAuthManager = UserAuthenticationManager()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .onOpenURL(perform: { url in
-                    GIDSignIn.sharedInstance.handle(url)
-                })
-                .onAppear {
-                    // TODO: 사용자가 이미 있을 경우 로그인 스킵
-                    GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
-                        
-                    }
-                }
-//                .environmentObject(userAuthManager)
+                .environmentObject(userAuthManager)
         }
     }
 }
--- QuoteHub/View/Onboarding/ContentView.swift
+++ QuoteHub/View/Onboarding/ContentView.swift
@@ -6,87 +6,18 @@
 //
 
 import SwiftUI
-import AuthenticationServices
-import GoogleSignIn
-import GoogleSignInSwift
-
-//struct ContentView: View {
-//    @EnvironmentObject var userAuthManager: UserAuthenticationManager
-//
-//    var body: some View {
-//        Group {
-//            if userAuthManager.isUserAuthenticated || userAuthManager.isOnboardingComplete {
-//                MainView().environmentObject(userAuthManager)
-//            } else {
-//                OnboardingView().environmentObject(userAuthManager)
-//            }
-//        }
-//        .onAppear(perform: userAuthManager.validateToken)
-//    }
-//}
 
 struct ContentView: View {
-    @Environment(\.colorScheme) var colorScheme
-    
+    @EnvironmentObject var userAuthManager: UserAuthenticationManager
+
     var body: some View {
-        VStack {
-            HStack(spacing: 20) {
-                Button(action: {
-                    handleSignInWithApple()
-                }) {
-                    Image("appleIcon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                }
-                .buttonStyle(PlainButtonStyle())
-
-                Button(action: {
-                    handleSignInWithGoogle()
-                }) {
-                    Image("googleIcon")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                }
-                .buttonStyle(PlainButtonStyle())
-
-                Button(action: {
-                    handleSignInWithKakao()
-                }) {
-                    Image("kakaoIcon")
-                        .resizable()
-                        .scaledToFit()
-                        .padding()
-                        .frame(width: 50, height: 50)
-                        .background(Color(hex: "ffe812"))
-                        .clipShape(.rect(cornerRadius: 5))
-                }
+        Group {
+            if userAuthManager.isUserAuthenticated || userAuthManager.isOnboardingComplete {
+                MainView().environmentObject(userAuthManager)
+            } else {
+                OnboardingView().environmentObject(userAuthManager)
             }
         }
-        .padding()
-    }
-    
-    private func handleSignInWithApple() {
-        print("APLLE LOGIN")
-    }
-    
-    private func handleSignInWithGoogle() {
-        print("GOOGLE LOGIN")
-        
-//        guard let rootViewController = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController else {return}
-//        
-//        GIDSignIn.sharedInstance.signIn(
-//            withPresenting: rootViewController) { signInResult, error in
-//                guard let result = signInResult else {
-//                    // Inspect error
-//                    return
-//                }
-//                // If sign in succeeded, display the app's main content View.
-//            }
-    }
-    
-    private func handleSignInWithKakao() {
-        print("KAKAO LOGIN")
+        .onAppear(perform: userAuthManager.validateToken)
     }
 }
