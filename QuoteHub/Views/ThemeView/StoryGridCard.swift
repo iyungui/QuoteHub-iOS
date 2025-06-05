cae3d64869e0a7be7dd803ada23320284f6b46ad
iyungui <rsdbddml@gmail.com>
2025-06-05 18:25:45 +0900

    [design] ThemeDetailView

---
+++ QuoteHub/Views/ThemeView/StoryGridCard.swift
@@ -0,0 +1,18 @@
+//
+//  StoryGridCard.swift
+//  QuoteHub
+//
+//  Created by 이융의 on 6/5/25.
+//
+
+import SwiftUI
+
+struct StoryGridCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryGridCard()
+}
---
+++ QuoteHub/Views/ThemeView/StoryListCard.swift
@@ -0,0 +1,18 @@
+//
+//  StoryListCard.swift
+//  QuoteHub
+//
+//  Created by 이융의 on 6/5/25.
+//
+
+import SwiftUI
+
+struct StoryListCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryListCard()
+}
