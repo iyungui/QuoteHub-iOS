8f54c23c1bf215c59ef99dabc08296005de24c3f
iyungui <rsdbddml@gmail.com>
2025-06-21 00:24:38 +0900

    [feat] BookStory 이벤트 시스템

---
+++ QuoteHub/Models/BookStoryNotifications.swift
@@ -0,0 +1,30 @@
+//
+//  BookStoryNotifications.swift
+//  QuoteHub
+//
+//  Created by 이융의 on 6/21/25.
+//
+
+import Foundation
+
+// MARK: - BookStory 이벤트 정의
+extension Notification.Name {
+    /// 북스토리 생성 이벤트
+    static let bookStoryCreated = Notification.Name("bookStoryCreated")
+    /// 북스토리 수정 이벤트
+    static let bookStoryUpdated = Notification.Name("bookStoryUpdated")
+    /// 북스토리 삭제 이벤트
+    static let bookStoryDeleted = Notification.Name("bookStoryDeleted")
+}
+
+// MARK: - UserInfo 키 정의
+extension Notification {
+    struct BookStoryKeys {
+        /// BookStory 객체 키 (생성, 수정 시 사용)
+        static let story = "story"
+        /// 삭제된 북스토리 ID 키 (삭제 시 사용)
+        static let deletedStoryId = "deletedStoryId"
+        /// 이전 북스토리 상태 키 (수정시 비교용)
+        static let previousStory = "previousStory"
+    }
+}
