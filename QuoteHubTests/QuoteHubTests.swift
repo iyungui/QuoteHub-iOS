366ebd6c5ff5e62bcac858e011be0d9f4ddcb92f
iyungui <rsdbddml@gmail.com>
2025-10-23 00:05:46 +0900

    [Test] add test target

---
+++ QuoteHubTests/QuoteHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteHubTests.swift
+//  QuoteHubTests
+//
+//  Created by iyungui on 10/23/25.
+//
+
+import Testing
+
+struct QuoteHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+    
+
+}
